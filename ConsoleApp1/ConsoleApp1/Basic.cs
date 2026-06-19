1025ecd477c6eae253d06f26f551b995bf085c99
M1rt3n <martinperezlarra@gmail.com>
2026-06-18 21:52:18 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Basic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Basic
+    {
+        private string _name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Nombre
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("El nombre no puede estar vacío o ser nulo.");
+                }
+                _name = value;
+            }
+        }
+        public virtual string Arrancar()
+        {
+            return $"Turno de {Nombre}.";
+        }
+    }
+
+    public class Mago : Basic
+    {
+        public int Magia { get; set; }
+        public Mago(string Nombre, int magia)
+        {
+            Magia = magia;
+        }
+        public override string Arrancar()
+        {
+            return $"[Turno de {Nombre}]. Hizo {Magia} puntos de daño mágico";
+        }
+    }
+    public class Guerrero : Basic
+    {
+        public int Fuerza { get; set; }
+        public Guerrero(string Nombre, int fuerza)
+        {
+            Fuerza = fuerza;
+        }
+        public override string Arrancar()
+        {
+            return $"[Turno de {Nombre}]. Hizo {Fuerza} puntos de daño físico";
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp1;
+using static ConsoleApp1.Guerrero;
+using static ConsoleApp1.Mago;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Basic> equipo = new List<Basic>();
+            equipo.Add(new Mago("Vladimir", 500));
+            equipo.Add(new Guerrero("Barlock", 175));
+            equipo.Add(new Guerrero("Seth", 200));
+            equipo.Add(new Guerrero("Frey", 150));
+
+        Console.WriteLine("\n--- Resumen del turno: ---");
+        foreach (Basic v in equipo)
+        {
+            Console.WriteLine(v.Arrancar());
+        }
+    }
+}
