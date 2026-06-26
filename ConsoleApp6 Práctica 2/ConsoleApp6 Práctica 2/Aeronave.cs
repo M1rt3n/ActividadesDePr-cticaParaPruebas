b6090d21df9b6b43288a8e83cd7b1729ec6a6355
M1rt3n <martinperezlarra@gmail.com>
2026-06-26 17:19:09 -0300

    Add files via upload

---
+++ ConsoleApp6 Práctica 2/ConsoleApp6 Práctica 2/Aeronave.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6_Práctica_2
+{
+    public class Aeronave
+    {
+        public int HorasVuelo { get; set; }
+        public int LímiteReporte { get; set; }
+        public int SituaciónReporte { get; set; }
+        public Aeronave(int horasvuelo)
+        {
+            HorasVuelo = horasvuelo;
+        }
+
+        public virtual string GenerarReporteMantenimiento()
+        {
+            return $"Total de horas voladas: {HorasVuelo}";
+        }
+    }
+
+    public class AviónComercial : Aeronave
+    {
+        public int PasajerosMáximos { get; set; }
+        public AviónComercial(int horasvuelo, int pasajerosmáximos) : base(horasvuelo)
+        {
+            PasajerosMáximos=pasajerosmáximos;
+        }
+        public override string GenerarReporteMantenimiento()
+        {
+            if (HorasVuelo >= 10000)
+            {
+                return $"Total de horas voladas: {HorasVuelo} | Cantidad de pasajeros máximos: {PasajerosMáximos} | Observaciones: Requiere una revisión de Turbinas Tipo A).";
+            }
+            return $"Total de horas voladas: {HorasVuelo} | Cantidad de pasajeros máximos: {PasajerosMáximos} | Observaciones: El avión se encuentra en condiciones";
+        }
+    }
+
+    public class Helicóptero : Aeronave
+    {
+        public int CiclosRotor { get; set; }
+        public Helicóptero(int horasvuelo, int ciclosrotor) : base(horasvuelo)
+        {
+            CiclosRotor = ciclosrotor;
+        }
+
+        public override string GenerarReporteMantenimiento()
+        {
+            LímiteReporte = 1000;
+            SituaciónReporte = CiclosRotor * HorasVuelo;
+            if (SituaciónReporte > LímiteReporte)
+            {
+                return $"Total de horas voladas: {HorasVuelo} | Ciclos de rotor: {CiclosRotor} | Observaciones: Alerta: Reemplazo de palas requerido";
+            }
+            return $"Total de horas voladas: {HorasVuelo} | Ciclos de rotor: {CiclosRotor} | Observaciones: El helicóptero se encuentra en condiciones";
+        }
+    }
+}
---
+++ ConsoleApp6 Práctica 2/ConsoleApp6 Práctica 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp6_Práctica_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Aeronave> equipo = new List<Aeronave>();
+        equipo.Add(new Helicóptero(100, 10));
+        equipo.Add(new AviónComercial(8900, 100));
+        equipo.Add(new Helicóptero(200, 30));
+        equipo.Add(new AviónComercial(10001, 120));
+
+        Console.WriteLine("\n--- Resumen del turno: ---");
+        foreach (Aeronave v in equipo)
+        {
+            Console.WriteLine(v.GenerarReporteMantenimiento());
+        }
+    }
+}
