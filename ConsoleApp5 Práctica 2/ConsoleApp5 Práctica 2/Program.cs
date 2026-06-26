c71cfae5a18e3750f0ac2ab52462539592cb6a9b
M1rt3n <martinperezlarra@gmail.com>
2026-06-26 17:02:06 -0300

    Add files via upload

---
+++ ConsoleApp5 Práctica 2/ConsoleApp5 Práctica 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp5_Práctica_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Transacción> equipo = new List<Transacción>();
+        equipo.Add(new Transferencia("jh91083r", 100,"Jorge.mercadolibre"));
+        equipo.Add(new PagoServicio("nfao1280", 10));
+        equipo.Add(new Transferencia("03ysda082",80,"Manolo.paypal"));
+
+        Console.WriteLine("\n--- Resumen del turno: ---");
+        foreach (Transacción v in equipo)
+        {
+            Console.WriteLine(v.Procesar("nfao1280", 500));
+            Console.WriteLine(new string('-', 50));
+        }
+    }
+}
---
+++ ConsoleApp5 Práctica 2/ConsoleApp5 Práctica 2/Transacción.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5_Práctica_2
+{
+    public class Transacción
+    {
+        private string _tokenSeguridad;
+
+        public string TokenSeguridad
+        {
+            get => _tokenSeguridad;
+            set
+            {
+                if (value != null && value.Length == 8)
+                    _tokenSeguridad = value;
+                else
+                    _tokenSeguridad = null;
+            }
+        }
+
+        public double PagoTotal { get; set; }
+        public Transacción(string tokenSeguridad)
+        {
+            TokenSeguridad = tokenSeguridad;
+        }
+
+        public virtual string Procesar(string tokenIngresado, double total)
+        {
+            if (string.IsNullOrEmpty(TokenSeguridad))
+            {
+                return "El token original debe tener exactamente 8 caracteres.";
+            }
+
+            if (tokenIngresado == TokenSeguridad)
+            {
+                PagoTotal = total;
+                return "Token aceptado";
+            }
+            else
+            {
+                return "Token denegado";
+            }
+        }
+    }
+
+    public class Transferencia : Transacción
+    {
+        public double Monto { get; set; }
+        public string CuentaDestino { get; set; }
+
+        public Transferencia(string tokenSeguridad, double monto, string cuentaDestino)
+            : base(tokenSeguridad)
+        {
+            Monto = monto;
+            CuentaDestino = cuentaDestino;
+        }
+
+        public override string Procesar(string tokenIngresado, double total)
+        {
+            string resultadoBase = base.Procesar(tokenIngresado, total);
+            if (resultadoBase != "Token aceptado") return resultadoBase;
+            if (Monto > 500000)
+            {
+                return "Requiere autorización presencial";
+            }
+
+            return $"Token aceptado, transferencia exitosa a {CuentaDestino} por ${Monto}.";
+        }
+    }
+
+    public class PagoServicio : Transacción
+    {
+        public int DiasVencido { get; set; }
+
+        public PagoServicio(string tokenSeguridad, int diasVencido)
+            : base(tokenSeguridad)
+        {
+            DiasVencido = diasVencido;
+        }
+
+        public override string Procesar(string tokenIngresado, double total)
+        {
+            string resultadoBase = base.Procesar(tokenIngresado, total);
+            if (resultadoBase != "Token aceptado") return resultadoBase;
+
+            if (DiasVencido > 0)
+            {
+                PagoTotal = total * 1.01*DiasVencido;
+
+                return $"Token aceptado, pago procesado con intereses. Total: ${PagoTotal:N2}";
+            }
+            else
+            {
+                return $"Token aceptado, pago procesado al día sin recargos. Total: ${PagoTotal:N2}";
+            }
+        }
+    }
+}
