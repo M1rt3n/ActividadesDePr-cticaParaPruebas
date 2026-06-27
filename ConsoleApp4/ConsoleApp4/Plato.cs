7005cde68fa7212c49736285eec3c269ec10ca22
M1rt3n <martinperezlarra@gmail.com>
2026-06-27 18:01:59 -0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Plato.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Plato
+    {
+        public string NombrePlato { get; set; }
+
+        public Plato(string nombreplato)
+        {
+            NombrePlato = nombreplato;
+        }
+        public virtual string Preparar()
+        {
+            return $"El plato {NombrePlato} se ha preparado exitosamente";
+        }
+    }
+
+    public class Pizza : Plato
+    {
+        public string TipoMasa { get; set; }
+        public Pizza(string nombreplato, string tipomasa) : base(nombreplato)
+        {
+            TipoMasa = tipomasa;
+        }
+        public override string Preparar()
+        {
+            return $"Tipo de masa para la pizza: {TipoMasa}, la pizza {NombrePlato} se ha preparado exitosamente";
+        }
+    }
+    public class Ensalada : Plato
+    {
+        public string TipoAderezo { get; set; }
+        public Ensalada(string nombreplato, string tipoaderezo) : base(nombreplato)
+        {
+            TipoAderezo = tipoaderezo;
+        }
+        public override string Preparar()
+        {
+            return $"Tipo de aderezo para la ensalada: {TipoAderezo}, la ensalada {NombrePlato} se ha preparado correctamente";
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp4;
+using System.Diagnostics.Metrics;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Plato> equipo = new List<Plato>();
+        equipo.Add(new Pizza("Hawaiana", "Batida"));
+        equipo.Add(new Ensalada("Marinera", "Aceite de oliva"));
+        equipo.Add(new Pizza("Capresse", "Fermentada"));
+        equipo.Add(new Ensalada("César", "Mayonesa"));
+        Console.WriteLine("\n--- Ensayo: ---");
+        foreach (Plato v in equipo)
+        {
+            Console.WriteLine(v.Preparar());
+        }
+    }
+}
