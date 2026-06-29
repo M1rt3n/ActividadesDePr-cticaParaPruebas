960d14272dc99375c6390f11de4aa25166054bb0
M1rt3n <martinperezlarra@gmail.com>
2026-06-29 15:10:51 -0300

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Juguete.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Juguete
+    {
+        public int NúmeroSerie { get; set; }
+
+        public Juguete(int númeroserie)
+        {
+            NúmeroSerie = númeroserie;
+        }
+        public virtual string ProbarJuguete()
+        {
+            return $"El juguete está siendo probado... Prueba completada";
+        }
+    }
+
+    public class Muñeca : Juguete
+    {
+        public string MaterialCabello { get; set; }
+        public Muñeca(int númeroserie, string materialcabello) : base(númeroserie)
+        {
+            MaterialCabello = materialcabello;
+        }
+        public override string ProbarJuguete()
+        {
+            return $"La muñeca está siendo probada | Tipo de cabello de la muñeca: {MaterialCabello}... Prueba completada";
+        }
+    }
+    public class AutoControl : Juguete
+    {
+        public int Frecuencia { get; set; }
+        public AutoControl(int númeroserie, int frecuencia) : base(númeroserie)
+        {
+            Frecuencia = frecuencia;
+        }
+        public override string ProbarJuguete()
+        {
+            return $"El autocontrol está siendo probado | Frecuencia del autocontrol: {Frecuencia}hz... Prueba completada";
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp7;
+using System.Diagnostics.Metrics;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Juguete> almacen = new List<Juguete>();
+        almacen.Add(new AutoControl(63, 16));
+        almacen.Add(new AutoControl(28, 32));
+        almacen.Add(new Muñeca(4, "Algodón"));
+        almacen.Add(new Muñeca(11, "Lana"));
+        Console.WriteLine("\n--- Ensayo: ---");
+        foreach (Juguete v in almacen)
+        {
+            Console.WriteLine(v.ProbarJuguete());
+        }
+    }
+}
