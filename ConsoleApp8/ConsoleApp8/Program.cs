9e1db25b671f00a7e4d5dfbbf23db46ba1de74ca
M1rt3n <martinperezlarra@gmail.com>
2026-06-29 21:02:47 -0300

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp8;
+using System.Diagnostics.Metrics;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Torreta> equipo = new List<Torreta>();
+        equipo.Add(new TorretaLaser("X:24 Y:0 Z:60", 100));
+        equipo.Add(new Lanzamisiles("X:18 Y:0 Z:62", 20));
+        equipo.Add(new Torreta("X:23 Y:2 Z:64" ));
+        equipo.Add(new Lanzamisiles("X:19 Y:0 Z:65", 0));
+        equipo.Add(new TorretaLaser("X:22 Y:-1 Z:63", 0));
+        Console.WriteLine("\n--- Ensayo: ---");
+        foreach (Torreta v in equipo)
+        {
+            Console.WriteLine(v.Disparar());
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Torreta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Torreta
+    {
+        public string Ubicación { get; set; }
+
+        public Torreta(string ubicación)
+        {
+            Ubicación = ubicación;
+        }
+        public virtual string Disparar()
+        {
+            return $"Torreta en {Ubicación} activada. Modo defensa establecido";
+        }
+    }
+
+    public class TorretaLaser : Torreta
+    {
+        public int CargaBatería { get; set; }
+        public TorretaLaser(string ubicación, int cargabatería) : base(ubicación)
+        {
+            CargaBatería = cargabatería;
+        }
+        public override string Disparar()
+        {
+            if (CargaBatería > 0)
+            {
+                return $"Torreta descargada. Recargando";
+            }
+            else
+            {
+                return $"Torreta laser en {Ubicación} activada | Carga de la batería: {CargaBatería}%. Modo defensa establecido";
+            }
+        }
+    }
+    public class Lanzamisiles : Torreta
+    {
+        public int MuniciónRestante { get; set; }
+        public Lanzamisiles(string ubicación, int municiónrestante) : base(ubicación)
+        {
+            MuniciónRestante = municiónrestante;
+        }
+        public override string Disparar()
+        {
+            if (MuniciónRestante > 0)
+            {
+                return $"Lanzamisiles sin munición. Requiere mantenimiento inmediato";
+            }
+            else
+            {
+                return $"Lanzamisiles en {Ubicación} activada | Munición restante: {MuniciónRestante} unidades. Modo defensa establecido";
+            }
+        }
+    }
+}
