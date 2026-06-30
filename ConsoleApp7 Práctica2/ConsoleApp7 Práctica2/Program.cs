bcda482d6c4e08b73808d1e18e18bf1156364a39
M1rt3n <martinperezlarra@gmail.com>
2026-06-30 20:44:54 -0300

    Add files via upload

---
+++ ConsoleApp7 Práctica2/ConsoleApp7 Práctica2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConsoleApp7_Práctica2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Subasta> equipo = new List<Subasta>();
+        equipo.Add(new SubastaRelámpago(31125, 16));
+        equipo.Add(new SubastaPremium(42500, 2250));
+        equipo.Add(new SubastaRelámpago(35750, 9));
+        equipo.Add(new SubastaPremium(50000, 2500));
+
+        Console.WriteLine("\n--- Resumen del turno: ---");
+        foreach (Subasta v in equipo)
+        {
+            Console.WriteLine(v.RegistrarOferta(45000));
+        }
+    }
+}
---
+++ ConsoleApp7 Práctica2/ConsoleApp7 Práctica2/Subasta.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7_Práctica2
+{
+    public class Subasta
+    {
+        private decimal _precioactual;
+        public decimal PrecioActual
+        {
+            get { return _precioactual; }
+            set { _precioactual = value; }
+        }
+
+        public Subasta(decimal precioactual)
+        {
+            PrecioActual = precioactual;
+        }
+
+        public virtual string RegistrarOferta(decimal Oferta)
+        {
+            if (Oferta > PrecioActual)
+            {
+                PrecioActual = Oferta;
+                return $"El precio actual está en {PrecioActual}";
+            }
+            else
+            {
+                return $"La oferta no puede ser aceptada: debe ser mayor a {PrecioActual}";
+            }
+        }
+    }
+
+    public class SubastaRelámpago : Subasta
+    {
+        private int _tiemporestantesegundos;
+        public int TiempoRestanteSegundos
+        {
+            get { return _tiemporestantesegundos; }
+            set { _tiemporestantesegundos = value; }
+        }
+        public SubastaRelámpago(int precioactual, int tiemporestantesegundos) : base(precioactual)
+        {
+            TiempoRestanteSegundos = tiemporestantesegundos;
+        }
+        public override string RegistrarOferta(decimal Oferta)
+        {
+            if (Oferta > PrecioActual)
+            {
+                PrecioActual = Oferta;
+                if (TiempoRestanteSegundos < 10)
+                {
+                    TiempoRestanteSegundos += 30;
+                    return $"¡Oferta de último segundo! Tiempo extendido a {TiempoRestanteSegundos} segundos. El precio actual está en {PrecioActual}";
+                }
+                else
+                {
+                    if (TiempoRestanteSegundos < 0)
+                    {
+                        return $"La subasta ya ha concluido, no se pueden ingresar más ofertas";
+                    }
+                    else
+                    {
+                        TiempoRestanteSegundos += 5;
+                        return $"Tiempo extendido a {TiempoRestanteSegundos} segundos. El precio actual está en {PrecioActual}";
+                    }
+                }
+            }
+            else
+            {
+                return $"La oferta no puede ser aceptada: debe ser mayor a {PrecioActual}";
+            }
+        }
+    }
+    public class SubastaPremium : Subasta
+    {
+        private decimal _montomínimoincremento;
+        public decimal MontoMínimoIncremento
+        {
+            get { return _montomínimoincremento; }
+            set { _montomínimoincremento = value; }
+        }
+        public SubastaPremium(int precioactual, int montomínimoincremento) : base(precioactual)
+        {
+            MontoMínimoIncremento = montomínimoincremento;
+        }
+        public override string RegistrarOferta(decimal Oferta)
+        {
+            if (Oferta > (PrecioActual + MontoMínimoIncremento))
+            {
+                PrecioActual = Oferta;
+                return $"El precio actual está en {PrecioActual}";
+            }
+            else
+            {
+                return $"La oferta no puede ser aceptada: debe ser mayor a {PrecioActual + MontoMínimoIncremento}";
+
+            }
+        }
+    }
+}
