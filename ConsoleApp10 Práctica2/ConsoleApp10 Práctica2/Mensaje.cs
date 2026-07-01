cd65a518bbff8bdfbca300706ea9c8d53b92d0b3
M1rt3n <martinperezlarra@gmail.com>
2026-06-30 22:29:22 -0300

    Add files via upload

---
+++ ConsoleApp10 Práctica2/ConsoleApp10 Práctica2/Mensaje.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp10_Práctica2
+{
+    public class Mensaje
+    {
+        private string _textooriginal;
+        public string TextoOriginal
+        {
+            get { return _textooriginal; }
+            set
+            {
+                foreach (char c in value)
+                {
+                    if (!char.IsLetter(c) && c != ' ')
+                    {
+                        throw new ArgumentException("No se admiten números ni caracteres especiales.");
+                    }
+                }
+                _textooriginal = value;
+            }
+        }
+        public Mensaje(string textooriginal)
+        {
+            TextoOriginal = textooriginal;
+        }
+        public virtual string Cifrar()
+        {
+            return TextoOriginal;
+        }
+    }
+    public class CifradoInvertido : Mensaje
+    {
+        public CifradoInvertido(string textooriginal) : base(textooriginal) { }
+
+        public override string Cifrar()
+        {
+            char[] Letras = TextoOriginal.ToCharArray();
+            Array.Reverse(Letras);
+            return new string(Letras);
+        }
+    }
+    public class CifradoDesplazamiento : Mensaje
+    {
+        public int ClaveNumérica { get; set; }
+        public CifradoDesplazamiento(string textooriginal, int clavenumérica) : base(textooriginal)
+        {
+            ClaveNumérica = clavenumérica;
+        }
+        public override string Cifrar()
+        {
+            string Resultado = "";
+            foreach (char c in TextoOriginal)
+            {
+                if (c == ' ')
+                {
+                    Resultado += " ";
+                }
+                else
+                {
+                    char LetraCifrada = (char)(c + ClaveNumérica);
+                    Resultado += LetraCifrada;
+                }
+            }
+            return Resultado;
+        }
+    }
+}
---
+++ ConsoleApp10 Práctica2/ConsoleApp10 Práctica2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ConsoleApp10_Práctica2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string texto = "Ejemplo";
+        List<Mensaje> equipo = new List<Mensaje>();
+
+        equipo.Add(new CifradoInvertido(texto));
+        equipo.Add(new CifradoDesplazamiento(texto, 1));
+        equipo.Add(new CifradoInvertido(texto));
+        equipo.Add(new CifradoDesplazamiento(texto, 3));
+        foreach (Mensaje v in equipo)
+        {
+            Console.WriteLine($"Clase: {v.GetType().Name}");
+            Console.WriteLine($"Resultado: {v.Cifrar()}");
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
